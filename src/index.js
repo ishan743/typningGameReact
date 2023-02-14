703c3195f91b83d367706864716311ee7acdc61e
Ishan Nandwal <thrillophilia@Thrillophilias-MacBook-Air-5.local>
2023-02-14 15:33:08 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DisplayProducts from './components/displayProducts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <DisplayProducts/>
   </React.StrictMode>
 );
 
